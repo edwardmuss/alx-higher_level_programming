2cbe859a1b00a83b55a6eee336b41bdbc49e79a8
edwardmuss <edwardmuss5@gmail.com>
2021-10-12 22:24:58 +0300

    some corrections

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 12-student.py
+# 11-student.py
 """Defines a class Student."""
 
 
@@ -31,3 +31,12 @@ class Student:
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 10-class_to_json.py
+# 8-class_to_json.py
 """Defines a Python class-to-JSON function."""
 
 
