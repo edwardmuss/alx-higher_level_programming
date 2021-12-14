b9d8d18ccca1e33cf99b47925e985d1ddf8ab55a
edwardmuss <edwardmuss5@gmail.com>
2021-12-14 16:19:50 +0300

    Js is data add

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
-#!/usr/bin/node
+// #!/usr/bin/node
 
-const dict = require('./101-data').dict;
+const dict = require('./101-data.js').dict;
 const newDict = {};
 
 Object.keys(dict).map(function (key) {
